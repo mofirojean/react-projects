cc835f88581ac6656aa8c92efee897077bbd8d57
mofirojean <mofirojean@gmail.com>
2024-04-21 22:32:52 +0100

    (LR) react movement

--- react basics/meme-generator/src/main.jsx
+++ react basics/meme-generator/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+/*
+Triggering: createRoot triggers a render and calling its render() function
+Rendering: React does this by calling your component
+after this react will commit it's to the DOM
+*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
