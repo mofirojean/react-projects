ecd61528026ce954a05baadda83b6edba3810971
mofirojean <mofirojean@gmail.com>
2024-04-25 15:40:18 +0100

    (LR) fixed header issue header

--- react basics/meme-generator/src/components/Header.jsx
+++ react basics/meme-generator/src/components/Header.jsx
@@ -1,5 +1,29 @@
 import {getImageUrl} from "../utils/image-url.utils.jsx";
 
+/**
+ * You can use Immer to mutate the state of a React object
+ * syntax:
+ *    @install: npm install use-immer
+ *
+ *    @import: import { useImmer } from 'use-immer'
+ *
+ *    @usage:
+ *    const [person, updatePerson] = useImmer({
+ *     name: 'Niki de Saint Phalle',
+ *     artwork: {
+ *       title: 'Blue Nana',
+ *       city: 'Hamburg',
+ *       image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+ *     }
+ *   });
+ *
+ *   function handleNameChange(e) {
+ *     updatePerson(draft => {
+ *       draft.name = e.target.value;
+ *     });
+ *   }
+ */
+
 export function Header() {
     return (
         <header className="navbar">
